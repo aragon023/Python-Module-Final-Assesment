7abb3c51a736cbd5ba8eb2130dd227fd01f5cb2c
aragon023 <mauricio.aragon@gmail.com>
2025-07-29 22:47:16 +0100

    Changes in articles and app.py Included comments section in article_detail.html Created CSS for comments section Update models in articles_data.py

--- app.py
+++ app.py
@@ -1,46 +1,72 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from project_data import projects_list  # This imports the projects list
-from articles_data import db, Article
+from articles_data import db, Article, Comment
 import random
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db.init_app(app)
 
+# Home Page
 @app.route('/')
 def index():
     all_articles = Article.query.all()
     featured_articles = random.sample(all_articles, min(3, len(all_articles)))
     return render_template("index.html", featured_articles=featured_articles)
 
+
+# About Page
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
+# Projects Page
 @app.route("/projects")
 def projects():
     return render_template("projects.html", projects=projects_list)
 
+# Tools Page
 @app.route("/tools")
 def tools():
     return render_template("tools.html")
 
+# Articles List Page
 @app.route("/articles")
 def articles():
     articles_list = Article.query.all()
     return render_template("articles.html", articles=articles_list)
 
-@app.route('/articles/<slug>')
+# Single Article + Comments
+@app.route('/articles/<slug>', methods=['GET', 'POST'])
 def article_detail(slug):
     article = Article.query.filter_by(slug=slug).first_or_404()
-    return render_template('article_detail.html', article=article)
+
+# Handle comment submission
+    if request.method == 'POST':
+        name = request.form['name']
+        content = request.form['content']  
+        
+        new_comment = Comment(
+            article_id=article.id,
+            name=name,
+            content=content
+        )
+        db.session.add(new_comment)
+        db.session.commit()
+        return redirect(url_for('article_detail', slug=slug))
+
+    comments = Comment.query.filter_by(article_id=article.id).order_by(Comment.created_at.desc()).all()
+    return render_template('article_detail.html', article=article, comments=comments)
 
 
+# Contact Page
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
 
+# Run App
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all() 
+        db.create_all()
     app.run(debug=True)
--- articles_data.py
+++ articles_data.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+  # Article Model
+
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
@@ -12,7 +14,21 @@ class Article(db.Model):
     image = db.Column(db.String(200), nullable=False)
     author = db.Column(db.String(100), default='Mauricio Aragon')
     published_on = db.Column(db.Date, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)  # full HTML body
+    content = db.Column(db.Text, nullable=False) 
 
     def __repr__(self):
         return f'<Article {self.title}>'
+
+  # Comment Model
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    article_id = db.Column(db.Integer, db.ForeignKey('article.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    article = db.relationship('Article', backref=db.backref('comments', lazy=True))
+
+    def __repr__(self):
+        return f'<Comment by {self.name} on Article {self.article_id}>'
